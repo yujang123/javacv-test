da91584245b9f1d067e519858a51e7c7a06bef79
yujang123 <yujang123@gmail.com>
2019-04-24 00:08:07 +0800

    视频按帧数转换为图片 or 图片转换成视频(todo)

---
+++ src/test/java/top/yujiangtao/ConvertToImageTest.java
@@ -0,0 +1,20 @@
+package top.yujiangtao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author yujan
+ * Date 2019/4/23/0023
+ * Time 22:55
+ **/
+public class ConvertToImageTest {
+
+    @Test
+    public void videoToFramers() {
+        ConvertToImage.videoToFramers("1.mp4");
+    }
+}
